920211e0b85fe3251a8753c4b1b78e5ce311d658
bajra <bajra.idabagus@karmagroup.com>
2022-05-17 15:10:47 +0800

    test

--- app/category/controller.js
+++ app/category/controller.js
@@ -2,7 +2,7 @@ module.exports = {
   index: async (req, res) => {
     try {
       res.render("index", {
-        title: "Category",
+        title: "Category Test",
       });
     } catch (err) {
       console.log(err);
